fc82ab2fc73324b2e3d79b23953992f7e5014213
Orealba <orealbas@gmail.com>
2022-01-21 12:03:08 +0000

    cambiosdenuevo

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
 import "./style.css";
+
+/*
+window.onload = function () {
+
+};
+const name= document.querySelector("#name");
+const submitbutton= document.querySelector("#submitButton")
+
+submitbutton.addEventListener('submit',(e) =>(
+e.preventDefault();
+if (name.ariaValueMax.length >= 8){
+    console.log("si es correcto")
+} else (
+    console.log("no es correcto"))
+)
+*/
