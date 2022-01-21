f233328d7ab907882a25b7e598aed9205119de42
Orealba <orealbas@gmail.com>
2022-01-21 13:11:18 +0000

    cambiosjavas

--- src/app.js
+++ src/app.js
@@ -15,3 +15,9 @@ if (name.ariaValueMax.length >= 8){
     console.log("no es correcto"))
 )
 */
+const myForm = document.getElementById("myForm");
+
+myForm.addEventListener("submit", e => {
+  e.preventDefault();
+  console.log("enviado");
+});
