f4e30b66e060d3b903bbec9369df8406da69d446
Orealba <orealbas@gmail.com>
2022-01-21 13:41:16 +0000

    javascript

--- src/app.js
+++ src/app.js
@@ -1,23 +1,12 @@
 import "./style.css";
 
-/*
-window.onload = function () {
+window.onload = function() {};
 
-};
-const name= document.querySelector("#name");
-const submitbutton= document.querySelector("#submitButton")
-
-submitbutton.addEventListener('submit',(e) =>(
-e.preventDefault();
-if (name.ariaValueMax.length >= 8){
-    console.log("si es correcto")
-} else (
-    console.log("no es correcto"))
-)
-*/
-const myForm = document.getElementById("myForm");
-
-myForm.addEventListener("submit", e => {
-  e.preventDefault();
-  console.log("enviado");
-});
+const name = document.querySelector("#name");
+const lastname = document.querySelector("#lastname");
+const phonenumber = document.querySelector("#phonenumber");
+const cardnumber = document.querySelector("#cardnumber");
+const expirationdate = document.querySelector("#expirationdate");
+const cvc = document.querySelector("#cvc");
+const buttonsubmit = document.querySelector("#buttonsubmit");
+const inputs = document.querySelector("#inputs");
