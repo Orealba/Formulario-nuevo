3886c61dec6b949b960173460e64e46899d8dc5e
Orealba <orealbas@gmail.com>
2022-01-21 14:40:00 +0000

    cambios

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import "./style.css";
 
 window.onload = function() {};
 
-const onlyletters = /^[A-Za-z/S*]+$/;
+const onlyletters = /^[A-Za-z\s*]+$/;
 const name = document.querySelector("#name");
 const lastname = document.querySelector("#lastname");
 const phonenumber = document.querySelector("#phonenumber");
@@ -11,6 +11,7 @@ const expirationdate = document.querySelector("#expirationdate");
 const cvc = document.querySelector("#cvc");
 const buttonsubmit = document.querySelector("#buttonsubmit");
 const inputs = document.querySelector("#inputs");
+const numbers = /\d/;
 
 name.addEventListener("focusout", e => {
   if (name.value.match(onlyletters)) {
@@ -19,5 +20,64 @@ name.addEventListener("focusout", e => {
   } else {
     name.classList.remove("isvalid");
     name.classList.add("isinvalid");
+  }
+});
+
+lastname.addEventListener("focusout", e => {
+  if (lastname.value.match(onlyletters)) {
+    lastname.classList.remove("isinvalid");
+    lastname.classList.add("isvalid");
+  } else {
+    lastname.classList.remove("isvalid");
+    lastname.classList.add("isinvalid");
+  }
+});
+
+phonenumber.addEventListener("focusout", e => {
+  if (phonenumber.value.match(onlyletters)) {
+    phonenumber.classList.remove("isinvalid");
+    phonenumber.classList.add("isvalid");
+  } else {
+    phonenumber.classList.remove("isvalid");
+    phonenumber.classList.add("isinvalid");
+  }
+});
+
+cardnumber.addEventListener("focusout", e => {
+  if (cardnumber.value.match(numbers)) {
+    cardnumber.classList.remove("isinvalid");
+    cardnumber.classList.add("isvalid");
+  } else {
+    cardnumber.classList.remove("isvalid");
+    cardnumber.classList.add("isinvalid");
+  }
+});
+expirationdate.addEventListener("focusout", e => {
+  if (expirationdate.value.match(onlyletters)) {
+    expirationdate.classList.remove("isinvalid");
+    expirationdate.classList.add("isvalid");
+  } else {
+    expirationdate.classList.remove("isvalid");
+    expirationdate.classList.add("isinvalid");
+  }
+});
+
+cvc.addEventListener("focusout", e => {
+  if (cvc.value.match(onlyletters)) {
+    cvc.classList.remove("isinvalid");
+    cvc.classList.add("isvalid");
+  } else {
+    cvc.classList.remove("isvalid");
+    cvc.classList.add("isinvalid");
+  }
+});
+
+buttonsubmit.addEventListener("focusout", e => {
+  if (buttonsubmit.value.match(onlyletters)) {
+    buttonsubmit.classList.remove("isinvalid");
+    buttonsubmit.classList.add("isvalid");
+  } else {
+    buttonsubmit.classList.remove("isvalid");
+    buttonsubmit.classList.add("isinvalid");
   }
 });
